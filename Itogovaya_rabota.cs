d3333f775a8c5d0fb3ffe287e68034a4ff14dfae
unknown <gushch75@gmail.com>
2022-06-17 14:57:08 +0300

    Формируем и заполняем начальный массив

--- Itogovaya_rabota.cs
+++ Itogovaya_rabota.cs
@@ -2,3 +2,21 @@
 //длина которых меньше либо равна 3 символам.Первоначальный массив можно ввести с клавиатуры, 
 // либо задать на страте выполнения алгоритма.
 // Прирешении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Создаем массив 
+internal class  Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Введите количество элементов массива ");
+        int n = Convert.ToInt32(Console.ReadLine());
+        string[] array = new string[n];
+        for (int i = 0; i < n; i++)
+        {
+            Console.WriteLine("Введите данные для заполнения строки массива ");
+            string? str = Console.ReadLine();
+            if(str == null)
+            {
+              str = "";
+            }
+            array[i] = str; 
+        }
